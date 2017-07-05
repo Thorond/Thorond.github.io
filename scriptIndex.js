782bfabd46f6505dd5811ae6c3b7d276bc9fb234
Thorond <arnaud.lefevre@telecom-sudparis.eu>
2017-07-05 15:31:04 +0200

    correction dimension du slide quand la résolution es tplus petite, de même pour les prestations, sous une certaine résolution, les textes seront déjà affiché; ajout de la couleur sur les flèches de l'index quand la souris est dessus

--- scriptIndex.js
+++ scriptIndex.js
@@ -26,12 +26,33 @@ function remonté(){
 	}
 }
 
+flècheMonté.addEventListener("mouseover", function(){ 
+	flècheMonté.style.backgroundColor = "#353535 ";
+});
+flècheMonté.addEventListener("mouseout", function(){ 
+	flècheMonté.style.backgroundColor = "transparent ";
+});
+
 
 //  *********************************************************
 
 var flècheDroite = document.querySelector(".flècheDroite");
 var flècheGauche = document.querySelector(".flècheGauche");
 var slide = document.querySelector("#slider-list");
+
+
+flècheDroite.addEventListener("mouseover", function(){ 
+	flècheDroite.style.backgroundColor = "#353535 ";
+});
+flècheGauche.addEventListener("mouseover", function(){ 
+	flècheGauche.style.backgroundColor = "#353535 ";
+});
+flècheDroite.addEventListener("mouseout", function(){ 
+	flècheDroite.style.backgroundColor = "transparent ";
+});
+flècheGauche.addEventListener("mouseout", function(){ 
+	flècheGauche.style.backgroundColor = "transparent ";
+});
 
 flècheDroite.addEventListener("click", function(){
 	slide.style.animationDuration = "0s";
@@ -43,9 +64,6 @@ flècheDroite.addEventListener("click", function(){
 		slide.style.marginLeft = "0px" ; 
 	}
 
-	// if ( slide.style.marginLeft == "0px" ){
-	// 	slide.style.animationDuration = "20s";
-	// }
 });
 
 flècheGauche.addEventListener("click", function(){
@@ -60,9 +78,6 @@ flècheGauche.addEventListener("click", function(){
 		slide.style.marginLeft = "-3038px" ; 
 	}
 
-	// if ( slide.style.marginLeft == "0px" ){
-	// 	slide.style.animationDuration = "20s";
-	// }
 });
 
 slide.addEventListener("mouseout" , function(){
@@ -70,3 +85,16 @@ slide.addEventListener("mouseout" , function(){
 	slide.style.animationDuration = "20s";
 });
 
+
+
+
+//  *********************************************************
+
+
+
+var tailleSlide = document.querySelector(".slider");
+var resolutionClient = document.body.clientWidth;
+
+var tailleEnPx = resolutionClient.toString() + "px"
+
+tailleSlide.style.width = tailleEnPx ;
--- tailleCasePrestation.js
+++ tailleCasePrestation.js
@@ -1,50 +1,101 @@
 var prestations = document.querySelectorAll(".casePrestation");
+var resolutionClient = document.body.clientWidth;
+var body = document.body ;
 
 
-for ( j = 0 ; j < prestations.length ; j++ ) { 
-	var temp = prestations[j].querySelector("ul");
-	if ( temp !== null ) {
-		temp.style.listStyleType = "none";
+if ( resolutionClient > 974 ){
+	for ( j = 0 ; j < prestations.length ; j++ ) { 
+		var temp = prestations[j].querySelector("ul");
+		if ( temp !== null ) {
+			temp.style.listStyleType = "none";
+		}
+		temp = prestations[j].querySelectorAll(".listePrestations");
+		if ( temp !== null ) {
+
+			for ( k = 0 ; k < temp.length ; k++) { temp[k].style.color = "transparent";}
+			
+		}
 	}
-	temp = prestations[j].querySelectorAll(".listePrestations");
-	if ( temp !== null ) {
 
-		for ( k = 0 ; k < temp.length ; k++) { temp[k].style.color = "transparent";}
-		
+	for ( i = 0 ; i < prestations.length ; i++ ) { 
+	    prestations[i].addEventListener("mouseover",function() { 
+
+	    	enleverEcriture(prestations);
+	    	
+	        this.classList.remove("col-md-1"); 
+	        this.classList.add("col-md-4");
+	    	this.classList.add("écritureCasePrestation") ;
+	    	this.classList.remove("écritureTransparenteCasePrestation") ;
+	    	this.querySelector(".paragraphe").classList.add("paragrapheCasePrestation");
+	    	var temp = this.querySelector("ul");
+			if ( temp !== null ) {
+				temp.style.listStyleType = "square";
+			}
+	    	temp = this.querySelectorAll(".listePrestations");
+			if ( temp !== null ) {
+				for ( k = 0 ; k < temp.length ; k++) { temp[k].style.color = "white";}
+			}
+	    });
+	}
+} else {
+	mettreEcriture(prestations);
+	
+}
+
+function enleverEcriture (pres) {
+	for ( j = 0 ; j < pres.length ; j++ ) { 
+		pres[j].classList.remove("col-md-4") ;
+		pres[j].classList.add("col-md-1") ;
+		pres[j].classList.remove("écritureCasePrestation") ;
+		pres[j].classList.add("écritureTransparenteCasePrestation") ;
+		pres[j].querySelector(".paragraphe").classList.remove("paragrapheCasePrestation");
+		var temp = pres[j].querySelector("ul");
+		if ( temp !== null ) {
+			temp.style.listStyleType = "none";
+		}
+		temp = pres[j].querySelectorAll(".listePrestations");
+		if ( temp !== null ) {
+
+			for ( k = 0 ; k < temp.length ; k++) { temp[k].style.color = "transparent";}
+			
+		}
 	}
 }
 
-for ( i = 0 ; i < prestations.length ; i++ ) { 
-    prestations[i].addEventListener("mouseover",function() { 
-    	for ( j = 0 ; j < prestations.length ; j++ ) { 
-    		prestations[j].classList.remove("col-md-4") ;
-    		prestations[j].classList.add("col-md-1") ;
-    		prestations[j].classList.remove("écritureCasePrestation") ;
-    		prestations[j].classList.add("écritureTransparenteCasePrestation") ;
-    		prestations[j].querySelector(".paragraphe").classList.remove("paragrapheCasePrestation");
-    		var temp = prestations[j].querySelector("ul");
-    		if ( temp !== null ) {
-    			temp.style.listStyleType = "none";
-    		}
-    		temp = prestations[j].querySelectorAll(".listePrestations");
-    		if ( temp !== null ) {
-
-    			for ( k = 0 ; k < temp.length ; k++) { temp[k].style.color = "transparent";}
-    			
-    		}
-    	}
-        this.classList.remove("col-md-1"); 
-        this.classList.add("col-md-4");
-    	this.classList.add("écritureCasePrestation") ;
-    	this.classList.remove("écritureTransparenteCasePrestation") ;
-    	this.querySelector(".paragraphe").classList.add("paragrapheCasePrestation");
-    	var temp = this.querySelector("ul");
+function mettreEcriture ( pres) {
+	for ( j = 0 ; j < pres.length ; j++ ) { 
+		pres[j].classList.add("écritureCasePrestation") ;
+		pres[j].classList.remove("écritureTransparenteCasePrestation") ;
+		pres[j].querySelector(".paragraphe").classList.add("paragrapheCasePrestation");
+		var temp = pres[j].querySelector("ul");
 		if ( temp !== null ) {
 			temp.style.listStyleType = "square";
 		}
-    	temp = this.querySelectorAll(".listePrestations");
+		temp = pres[j].querySelectorAll(".listePrestations");
 		if ( temp !== null ) {
+
 			for ( k = 0 ; k < temp.length ; k++) { temp[k].style.color = "white";}
+			
 		}
-    });
+	}
 }
+
+function écritureSelonRésolution () {
+	if ( resolutionClient > 974 ){
+		enleverEcriture(prestations);
+		prestations[0].classList.remove("col-md-1"); 
+	    prestations[0].classList.add("col-md-4");
+	} else {
+		mettreEcriture(prestations)
+	}
+}
+
+
+body.addEventListener("mouseover",function() { 
+	if ( document.body.clientWidth !== resolutionClient ) {
+		resolutionClient = document.body.clientWidth;
+		écritureSelonRésolution();
+	}
+});
+
+
