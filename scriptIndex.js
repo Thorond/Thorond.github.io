a273d1f967d3d3fd0cb0bfddef01dd443e51decf
Thorond <arnaud.lefevre@telecom-sudparis.eu>
2017-07-06 11:34:26 +0200

    correction bug firefox, modifications mineurs

--- scriptIndex.js
+++ scriptIndex.js
@@ -4,26 +4,38 @@ var remonte = false;
 var inter ;
 
 
-section.addEventListener("click", function(){
+section.addEventListener("click", function(){ // lorsque l'utilisateur clique sur un endroit de la page, le défilement s'arrête 
 	if ( remonte ) {
 		clearInterval(inter);
 		remonte = false ;
 	}
 });
 
-flècheMonté.addEventListener("click", function() { 
+flècheMonté.addEventListener("click", function() {  // initiale le processus de défilement
 	remonte = true;
 	inter = setInterval("remonté()",1);
 });
 
 function remonté(){
-	if ( remonte ) {
-		document.body.scrollTop = document.body.scrollTop - 3 ;
+	if ( document.body.scrollTop != 0 ) { // pour chrome et autre 
+		if ( remonte ) {
+			document.body.scrollTop = document.body.scrollTop - 3 ;
+		}
+		if ( document.body.scrollTop < 1 ){
+			remonte = false ;
+			clearInterval( inter );
+		}
+	} 
+	if ( document.documentElement.scrollTop != 0 ) { // pour firefox , ici le fait de monter est saccadé .. 
+		if ( remonte ) {
+			document.documentElement.scrollTop = document.documentElement.scrollTop - 2 ;
+		}
+		if ( document.documentElement.scrollTop < 1 ){
+			remonte = false ;
+			clearInterval( inter );
+		}
 	}
-	if ( document.body.scrollTop < 1 ){
-		remonte = false ;
-		clearInterval( inter );
-	}
+	
 }
 
 flècheMonté.addEventListener("mouseover", function(){ 
